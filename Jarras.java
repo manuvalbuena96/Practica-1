9aecfe13812e6acea1c1a53fae8e49269299c097
manuvalbuena96 <manu3d1996@gmail.com>
2016-04-03 18:25:49 +0200

    Errores Corregidos

--- Jarras.java
+++ Jarras.java
@@ -11,18 +11,6 @@ public class Jarra {
 		this.capacidad=capacidadInicial;
 	}
 //  metodos
-<<<<<<< HEAD
-<<<<<<< HEAD
-        public void llena(){
-		this.cantidad=this.capacidad;
-	}
-	public void vacia(){
-		this.cantidad=0;
-	}
-
-=======
-              1
-              2
               
      public void llenaDesde(Jarra j){
          while((this.cantidad<this.capacidad)&&(j.cantidad>0)){
@@ -34,16 +22,6 @@ public class Jarra {
         public String toString(){
           return ("("+this.capacidad+","+this.cantidad+")");
          }
->>>>>>> e507090c5a5b4e3c9a9143ab6b84267fef714630
-=======
-        public void llena(){
-		this.cantidad=this.capacidad;
-	}
-public void vacia(){
-		this.cantidad=0;
-	}
-
->>>>>>> cf7c7216a60faca6c00685a670fd2b43fcbf1e7d
 
         public void metodoComunitario(){ 
 // Comentario incluido para pruebas 
